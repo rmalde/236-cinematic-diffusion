d3f8c77959ff2f100ddc7488e748a6bdd88b332a
Ronak Malde <rmalde@stanford.edu>
2023-11-17 02:48:00 +0000

    pretrain lora

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ accelerate launch train_text_to_image_lora.py \
   --gradient_accumulation_steps=4 \
   --gradient_checkpointing \
   --mixed_precision="fp16" \
-  --max_train_steps=15000 \
-  --learning_rate=1e-05 \
+  --max_train_steps=1500 \
+  --learning_rate=1e-04 \
   --max_grad_norm=1 \
   --lr_scheduler="constant" --lr_warmup_steps=0 \
   --output_dir=$OUTPUT_DIR
